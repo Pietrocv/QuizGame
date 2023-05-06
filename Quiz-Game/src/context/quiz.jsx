d1bd568cddee7b0a5a1037b9cc6419d359fd7c6e
Pietrocvisentin@gmail.com <Pietrocvisentin@gmail.com>
2023-05-05 22:39:15 -0300

    Definindo os estágios iniciais

--- Quiz-Game/src/context/quiz.jsx
+++ Quiz-Game/src/context/quiz.jsx
@@ -2,7 +2,15 @@
     Arquvio destinado a trabalhar com a lógica, aqui é onde podemos alterar os estados da aplicação.
 */
 
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import questions from '../data/questions'
+
+const STAGES  = ["Start", "Playing", "End"]
+
+const inicialSTATE = {
+    gameStage: STAGES[0],
+    questions
+}
 
 export const QuizContext = createContext()
 
